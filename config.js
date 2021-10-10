e1aafd888d22c69ef07f00808fa6ff105e49bf48
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-10-10 14:26:40 +0530

    Create config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 KAWEESHA CHAMOD
+
+CODDED BY KAWEESHA CHAMOD
 
 Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+you may not use this file except in compliance with the License.
+StefaniePublic - KAWEESHA CHAMOD
 */
 
 const { Sequelize } = require('sequelize');
